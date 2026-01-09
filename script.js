2f475f36bf9b1f5a00c4ec4126926a84c2f2efaa
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-01-09 11:31:02 +0500

    9 jan

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ var a=10;
 console.log(a);
 var b=19;
 console.log(b);
+let n = 5;
+let pattern = "";
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= i; j++) {
+    pattern += "*";
+  }
+  pattern += "\n";
+}
+
+console.log(pattern);

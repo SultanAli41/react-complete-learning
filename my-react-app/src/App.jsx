0e15a3c57c532e25a7d71cc21a1f0f45a4740122
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-05-03 22:10:37 +0500

    React

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,9 +3,10 @@ import Card from './components/card'
 const App = () => {
   return (
     <div> 
-   <Card /> 
+   <Card user="Ali" img ="https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D"/>
+   <Card user="Asmar" img ="https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D" />
+    <Card user="Haider" img ="https://images.unsplash.com/photo-1639747280804-dd2d6b3d88ac?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D" /> 
     </div>
   )
 }
-
 export default App;
--- my-react-app/src/components/card.jsx
+++ my-react-app/src/components/card.jsx
@@ -1,7 +1,7 @@
-function Card(){
+function Card(prop){
     return <div className='Card'>
-            <img src="https://plus.unsplash.com/premium_photo-1707816501228-1d814ad62d7b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Ym95fGVufDB8fDB8fHww" alt="" />
-            <h1>Sultan Ali</h1>
+            <img src={prop.img} alt="" />
+            <h1>{prop.user}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             <button>View Profile</button>
           </div>

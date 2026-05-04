4e16db7deaaa508f8d0de4b9457d4bad53cb78a9
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-05-04 23:30:36 +0500

    Prop Project using for each and map

--- Props-Project/src/App.jsx
+++ Props-Project/src/App.jsx
@@ -1,33 +1,26 @@
 import React from 'react'
-import {Bookmark} from 'lucide-react'
+import Card from './components/card'
 function App() {
+
+ const arr =[
+  {
+user:"Ali",
+age:21
+ },
+ {
+user:"Haider",
+age:23
+ },
+ {
+user:"Ahmad",
+age:27
+ }];
+ {arr.forEach(function(val){
+    console.log(val.user , val.age);
+   })}
+
   return (
     <div className='parent'>
-    <div className="card"> 
-
-       <div className="top">
-          <img src="https://i.pinimg.com/1200x/5a/62/70/5a62706bc5603694b1bd08acc40d3096.jpg" alt="" />
-          <button className='s-btn' >Save <Bookmark /> </button>
-       </div>
-
-              <div className="center">
-               <h3>Amazon<span>5 days ago</span></h3>
-               <h2>Senior UI/UX Designer</h2>
-               <div className='tags'>
-                <h4>Part Time</h4>
-                <h4>Senior Level</h4>
-               </div>
-              </div>
-
-
-         <div className="bottom">
-           <div>
-           <h3>120/hr</h3>
-           <p>Islamabad,Pakistan</p>
-           </div>
-           <button>Apply Now </button>
-         </div>
-         </div>
     </div>
   )
 }

71f6e9eb500d31a8c4173e27f5918f8095614043
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-05-09 12:47:16 +0500

    CSS

---
+++ CSS/src/Components/Button/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const button = () => {
+  return (
+    <div className='btn'>
+      
+    </div>
+  )
+}
+
+export default button
---
+++ CSS/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+      <h1>Hello</h1>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Header

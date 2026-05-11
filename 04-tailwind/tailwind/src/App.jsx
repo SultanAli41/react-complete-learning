42ac002eb7ddc964c2c777faa1c061a8e25217f7
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-05-11 21:44:12 +0500

    Tailwind

---
+++ 04-tailwind/tailwind/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-sky-500 p-10 text-white">
+      <h1 className="text-3xl font-bold">Tailwind is working!</h1>
+    </div>
+  )
+}
+
+// Make sure THIS line exists!
+export default App

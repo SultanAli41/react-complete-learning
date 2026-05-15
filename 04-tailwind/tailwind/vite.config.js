395af818748bb39de2fd7ed76713cae1e293a4ca
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-05-15 07:50:45 +0500

    tailwind

--- 04-tailwind/tailwind/vite.config.js
+++ 04-tailwind/tailwind/vite.config.js
@@ -6,6 +6,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <--- YOU MUST ADD THIS LINE HERE
+    tailwindcss(),
   ],
 })

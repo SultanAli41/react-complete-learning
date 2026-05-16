afda6d4d549fe609e0e0193c39d0911c13360c02
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-05-16 12:29:43 +0500

    Functions

--- 06-functions/06-functions/src/App.jsx
+++ 06-functions/06-functions/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
 function App() {
+  function btnClicked(){
+    console.log("Button is clicked")
+  }
+  function mouseEnter(){
+    console.log("Mouse is Entered")
+  }
   return (
     <div>
-      Hello
+<input  onChange={function(elem){
+console.log(elem.target.value);
+}} type="text" placeholder='Enter your Name' />
+
+<div className='box' onMouseMove={function(elem){console.log(elem.clientY);}}></div>
+
     </div>
+
   )
 }
 

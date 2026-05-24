a9b9b326c57ec0b82db22ff80929cfd51984f6ba
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-05-24 08:03:42 +0500

    Notes App

---
+++ 11-Notes-App/Notes-App/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder='Enter Notes Heading'/>
+        <textarea name="" id="" placeholder='Enter details here'></textarea>
+        <button>Add Notes</button>
+      </form>
+    </div>
+  )
+}
+
+export default App

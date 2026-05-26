1fac3d2475167bd778e755de2cd05a4a1dfcf845
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-05-26 23:13:01 +0500

    Notes App

--- 11-Notes-App/Notes-App/src/App.jsx
+++ 11-Notes-App/Notes-App/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
 const App = () => {
+  function formhandler(e){
+   e.preventDefault();
+   console.log("Submited");
+  }
   return (
-    <div>
-      <form action="">
-        <input type="text" placeholder='Enter Notes Heading'/>
-        <textarea name="" id="" placeholder='Enter details here'></textarea>
-        <button>Add Notes</button>
+    <div className='bg-zinc-950 h-screen w-screen '>
+      <div className='h-1/2 w-1/3 flex flex-col '>
+<form className='flex flex-col justify-between' 
+onSubmit={(e)=>{formhandler(e)}}>
+        <input type="text" placeholder='Enter Notes Heading' className='h-[50px] w-[70%] border-white-100 text-xl rounded-2xl mt-5 bg-white py-1 px-1' />
+        <textarea name="" id="" placeholder='Enter details here' className='h-[50px] w-[70%] border-white-100 text-xl rounded-2xl mt-5 bg-white py-1 px-1' ></textarea>
+        <button type='submit' className='h-[50px] w-[70%] border-white-100 text-xl rounded-2xl bg-blue-600 text-white  mt-5 py-1 px-1' >Add Notes</button>
       </form>
+      </div>
+      
     </div>
   )
 }

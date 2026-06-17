75bdc4eac032627fa31f045380ff50a065994b6b
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-06-17 23:54:39 +0500

    Use-Effect

--- 14-Use-Effect/use-effect/src/App.jsx
+++ 14-Use-Effect/use-effect/src/App.jsx
@@ -2,19 +2,26 @@ import React, { useEffect, useState } from 'react'
 
 function App() {
   //This run side by side 
-  const [num , setNum]=useState(0);
+ /* const [num , setNum]=useState(0);
     const [num2 , setNum2]=useState(100);
   useEffect(function(){
     console.log("use effect is working");
-  },[num]);
+  },[num,num2]);*/
+  function aChanging(){
+   console.log("A Change hoo gaya")
+  }
+  function bChanging(){
+     console.log("B Change hoo gaya")
+  }
   return (
     <div>
-      <h1>{num}</h1>
+     {/*<h1>{num}</h1>
       <h1>{num2}</h1>
      <button 
      onMouseEnter={()=>{setNum(num + 1)}}
      onMouseLeave={()=>{setNum2(num2 + 10)}}
-     >count</button>
+     >count</button>*/} 
+
     </div>
   )
 }

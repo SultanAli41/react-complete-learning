fa6bbde8fb6f0c617dafe05d5819e0142aa12c29
SultanAli41 <Sultanalibalouch512@gmail.com>
2026-06-18 23:29:39 +0500

    Use-Effect

--- 14-Use-Effect/use-effect/src/App.jsx
+++ 14-Use-Effect/use-effect/src/App.jsx
@@ -7,12 +7,17 @@ function App() {
   useEffect(function(){
     console.log("use effect is working");
   },[num,num2]);*/
+  const[a,seta] = useState(0);
+    const[b,setb] = useState(100);
   function aChanging(){
    console.log("A Change hoo gaya")
   }
   function bChanging(){
      console.log("B Change hoo gaya")
   }
+  useEffect(function(){
+    console.log("A chal gaya")
+  },[a,b])
   return (
     <div>
      {/*<h1>{num}</h1>
@@ -21,7 +26,16 @@ function App() {
      onMouseEnter={()=>{setNum(num + 1)}}
      onMouseLeave={()=>{setNum2(num2 + 10)}}
      >count</button>*/} 
-
+     <h1>Value of Btn1 is {a}</h1>
+      <h1>Value of Btn1 is {b}</h1>
+     <button onClick={
+      ()=>{seta(a+ 1)}
+     }>Btn 1</button>
+      
+      <button
+      onClick={()=>{ setb(b+10) }}
+      >Btn 2</button>
+      
     </div>
   )
 }
